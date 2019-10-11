3b81a6b022b4a29b548a1cffdbf87b6a2790014a
Emery Tate <tateejt@gmail.com>
2019-10-11 02:34:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,8 +11,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift "wow"
-end
+  array.unshift ["wow"]
+  end
 
 def remove_element_from_end_of_array(array)
   array.pop "arrays!"
